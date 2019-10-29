701ebf1fc60406836a2851f4cc1cd0b8878710c3
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-29 16:22:22 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+	attr_accessor :name, :artist, :genre
+	@@count = 0
+	@@artists = []
+	@@genres = []
+
+	def initialize(name, artist, genre)
+		@name = name
+		@artist = artist
+		@genre = genre
+
+		@@count+=1
+		@@artists << artist
+		@@genres << genre
+	end
+
+	def self.count
+		@@count
+	end
+
+	def self.artists
+		@@artists.uniq
+	end
+
+	def self.genres
+		@@genres.uniq
+	end
+
+	def self.genre_count
+		genreCount = {}
+		@@genres.each do |genre|
+			if genreCount.key?(genre)
+				genreCount[genre] += 1
+			else
+				genreCount[genre] = 1
+			end
+		end
+		genreCount
+	end
+
+	def self.artist_count
+		artistCount = {}
+		@@artists.each do |artist|
+			if artistCount.key?(artist)
+				artistCount[artist] += 1
+			else
+				artistCount[artist] = 1
+			end
+		end
+		artistCount
+	end
+end
